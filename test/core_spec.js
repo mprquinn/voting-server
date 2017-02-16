5d134c8371cfb3a9e529af993e8f44cb31546760
Mike Tomothy Q <mike@simplesimple.ca>
2017-02-16 10:05:55 -0700

    simplify cote tally code. no need to include whole object

--- test/core_spec.js
+++ test/core_spec.js
@@ -97,20 +97,14 @@ describe('application logic', () => {
 
     it('creates a tally for the voted entry' () => {
       const state = Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later')
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 Days Later')
       });
       const nextState = vote(state, 'Trainspotting');
       expect(nextState).to.equal(Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later'),
-          tally: Map({
-            'Trainspotting': 1
-          })
-        }),
-        entries: List()
+        pair: List.of('Trainspotting', '28 Days Later'),
+        tally: Map({
+          'Trainspotting': 1
+        })
       }));
     });
 
